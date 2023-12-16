bf71c665920da008d5078f454c040b86aace0aa4
Artem Striver <dog-di@mail.ru>
2023-12-16 09:04:21 +0500

    FastAPI app: add authorization.

---
+++ src/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+from src.auth.base_config import fastapi_users, auth_backend
+from src.auth.schemas import UserRead, UserCreate
+
+app = FastAPI(
+    title="Trading App"
+)
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth",
+    tags=["Auth"],
+)
+
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["Auth"],
+)
